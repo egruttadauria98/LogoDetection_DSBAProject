667d331821ad696c22a78f25536e97e2007510de
Rebecca Solcia <74981737+RebSolcia@users.noreply.github.com>
2021-10-26 20:01:59 +0200

    Catch up getting the TF2API work

--- training_process/training/training.py
+++ training_process/training/training.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
-import pathlib
-from git import Repo
-
 """
 We will take the following steps to implement YOLOv4 on our custom data:
 * Install TensorFlow2 Object Detection Dependencies
@@ -14,25 +10,10 @@ We will take the following steps to implement YOLOv4 on our custom data:
 * Use Trained TensorFlow2 Object Detection For Inference on Test Images
 """
 
-models_folder = os.path.join(os.getcwd(), "models")
 
-# Clone the tensorflow models repository if it doesn't already exist in this folder
-if "models" in pathlib.Path.cwd().parts:
-    while "models" in pathlib.Path.cwd().parts:
-        os.chdir('..')
-
-elif not pathlib.Path('models').exists():
-    os.mkdir("./models")
-    repo = Repo.clone_from(
-        'http://RebSolcia:Clementinabookie18121998!@github.com/tensorflow/models.git',
-        models_folder,
-        depth=1,
-        branch='master',
-    )
 
 # Commented out IPython magic to ensure Python compatibility.
 # # Install the Object Detection API
-
 
 # %%bash
 # cd models/research/
@@ -40,7 +21,6 @@ elif not pathlib.Path('models').exists():
 # cp object_detection/packages/tf2/setup.py .
 # python -m pip install .
 
-# Commented out IPython magic to ensure Python compatibility.
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -66,103 +46,9 @@ from object_detection.builders import model_builder
 
 # %matplotlib inline
 
-#run model builder test
-!python /content/models/research/object_detection/builders/model_builder_tf2_test.py
-
-def load_image_into_numpy_array(path):
-  """Load an image from file into a numpy array.
-
-  Puts image into numpy array to feed into tensorflow graph.
-  Note that by convention we put it into a numpy array with shape
-  (height, width, channels), where channels=3 for RGB.
-
-  Args:
-    path: a file path.
-
-  Returns:
-    uint8 numpy array with shape (img_height, img_width, 3)
-  """
-  img_data = tf.io.gfile.GFile(path, 'rb').read()
-  image = Image.open(BytesIO(img_data))
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
-def plot_detections(image_np,
-                    boxes,
-                    classes,
-                    scores,
-                    category_index,
-                    figsize=(12, 16),
-                    image_name=None):
-  """Wrapper function to visualize detections.
-
-  Args:
-    image_np: uint8 numpy array with shape (img_height, img_width, 3)
-    boxes: a numpy array of shape [N, 4]
-    classes: a numpy array of shape [N]. Note that class indices are 1-based,
-      and match the keys in the label map.
-    scores: a numpy array of shape [N] or None.  If scores=None, then
-      this function assumes that the boxes to be plotted are groundtruth
-      boxes and plot all boxes as black with no classes or scores.
-    category_index: a dict containing category dictionaries (each holding
-      category index `id` and category name `name`) keyed by category indices.
-    figsize: size for the figure.
-    image_name: a name for the image file.
-  """
-  image_np_with_annotations = image_np.copy()
-  viz_utils.visualize_boxes_and_labels_on_image_array(
-      image_np_with_annotations,
-      boxes,
-      classes,
-      scores,
-      category_index,
-      use_normalized_coordinates=True,
-      min_score_thresh=0.8)
-  if image_name:
-    plt.imsave(image_name, image_np_with_annotations)
-  else:
-    plt.imshow(image_np_with_annotations)
-
-"""# Prepare Tensorflow 2 Object Detection Training Data
-
-
-Roboflow automatically creates our TFRecord and label_map files that we need!
-
-**Generating your own TFRecords the only step you need to change for your own custom dataset.**
-
-Because we need one TFRecord file for our training data, and one TFRecord file for our test data, we'll create two separate datasets in Roboflow and generate one set of TFRecords for each.
-
-To create a dataset in Roboflow and generate TFRecords, follow [this step-by-step guide](https://blog.roboflow.ai/getting-started-with-roboflow/).
-
-![](https://i.imgur.com/ZwMdcbY.png)
-"""
-
-#follow the link below to get your download code from from Roboflow
-!pip install -q roboflow
-from roboflow import Roboflow
-rf = Roboflow(model_format="tfrecord", notebook="roboflow-tf2-od")
-
-#Downloading data from Roboflow
-from roboflow import Roboflow
-rf = Roboflow(api_key="YOUR_API_KEY")
-project = rf.workspace().project("YOUR_PROJECT")
-dataset = project.version("YOUR_VERSION").download("tfrecord")
-
-# NOTE: Update these TFRecord names from "cells" and "cells_label_map" to your files!
-test_record_fname = dataset.location + '/test/logo.tfrecord'
-train_record_fname = dataset.location + '/train/logo.tfrecord'
-label_map_pbtxt_fname = dataset.location + '/train/logo_label_map.pbtxt'
-
-"""# Configure Custom TensorFlow2 Object Detection Training Configuration
-
-
-
-
-> In this section you can specify any model in the [TF2 OD model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2_detection_zoo.md) and set up your training configuration.
-
-
-"""
+test_record_fname = os.path.join(os.getcwd(),"pictures/test/logo.tfrecord")
+train_record_fname = os.path.join(os.getcwd(),"pictures/train/logo.tfrecord")
+label_map_pbtxt_fname = os.path.join(os.getcwd(),"pictures/train/logo_label_map.pbtxt")
 
 ##change chosen model to deploy different models available in the TF2 object detection zoo
 MODELS_CONFIG = {
@@ -196,8 +82,8 @@ MODELS_CONFIG = {
 #if you want to scale up tot larger efficientdet models you will likely need more compute!
 chosen_model = 'efficientdet-d0'
 
-num_steps = 40000 #The more steps, the longer the training. Increase if your loss function is still decreasing and validation metrics are increasing. 
-num_eval_steps = 500 #Perform evaluation after so many steps
+num_steps = 3000 #The more steps, the longer the training. Increase if your loss function is still decreasing and validation metrics are increasing.
+num_eval_steps = 50 #Perform evaluation after so many steps
 
 model_name = MODELS_CONFIG[chosen_model]['model_name']
 pretrained_checkpoint = MODELS_CONFIG[chosen_model]['pretrained_checkpoint']
@@ -208,6 +94,7 @@ batch_size = MODELS_CONFIG[chosen_model]['batch_size'] #if you can fit a large b
 #download pretrained weights
 # %mkdir /content/models/research/deploy/
 # %cd /content/models/research/deploy/
+
 import tarfile
 download_tar = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/' + pretrained_checkpoint
 
