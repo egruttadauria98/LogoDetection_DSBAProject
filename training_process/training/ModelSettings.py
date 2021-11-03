5cb2b38a084ff33da051e1a96cd70d50cdc7e137
Rebecca Solcia <74981737+RebSolcia@users.noreply.github.com>
2021-11-03 21:39:38 +0100

    Commit to catch up with main branch

--- training_process/training/ModelSettings.py
+++ training_process/training/ModelSettings.py
@@ -18,18 +18,38 @@ def Model_Setting():
             'pretrained_checkpoint': 'efficientdet_d2_coco17_tpu-32.tar.gz',
             'batch_size': 16
         },
-            'efficientdet-d3': {
+        'efficientdet-d3': {
             'model_name': 'efficientdet_d3_coco17_tpu-32',
             'base_pipeline_file': 'ssd_efficientdet_d3_896x896_coco17_tpu-32.config',
             'pretrained_checkpoint': 'efficientdet_d3_coco17_tpu-32.tar.gz',
-            'batch_size': 16
+            'batch_size': 5
         },
-            'faster_rcnn_inception_resnet_v2':{
+        'faster_rcnn_inception_resnet_v2':{
             'model_name': 'faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8',
             'base_pipeline_file': 'faster_rcnn_resnet152_v1_1024x1024_coco17_tpu-8.config',
             'pretrained_checkpoint': 'faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8.tar.gz',
             'batch_size': 16
-        },        
+        },
+        'centernet_hg104':{
+            'model_name': 'centernet_hg104_512x512_coco17_tpu-8',
+            'base_pipeline_file': 'center_net_deepmac_512x512_voc_only_tpu-32.config',
+            'pretrained_checkpoint': 'centernet_hg104_512x512_coco17_tpu-8.tar.gz',
+            'batch_size': 16
+        },
+        'faster_rcnn_inception_resnet_v2_640x640_coco17_tpu-8':{
+            'model_name': 'faster_rcnn_inception_resnet_v2_640x640_coco17_tpu-8',
+            'base_pipeline_file': 'faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.config',
+            'pretrained_checkpoint': 'faster_rcnn_inception_resnet_v2_640x640_coco17_tpu-8.tar.gz',
+            'batch_size': 16
+        },
+        'ssd_resnet152_v1':{
+            'model_name': 'ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8',
+            'base_pipeline_file': 'ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8.config',
+            'pretrained_checkpoint': 'ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8.tar.gz',
+            'batch_size': 16
+        },
+        
+
 
     }
     
