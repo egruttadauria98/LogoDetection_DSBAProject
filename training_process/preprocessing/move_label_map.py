50675bb5404416bde8fea007352aff66c79e65e7
RebSolcia <rebecca.solcia@icloud.com>
2021-11-08 22:23:53 +0000

    Remove label map

--- training_process/preprocessing/move_label_map.py
+++ training_process/preprocessing/move_label_map.py
@@ -5,6 +5,8 @@ source_file = "./logos_label_map.pbtxt"
 splits = ["train", "valid", "test"]
 
 for folder in os.listdir("./tfrecords"):
+    if not "Batch" in folder:
+        continue
     for split in splits:
         dest_dir = f"./tfrecords/{folder}/{split}"
         shutil.copy(source_file, dest_dir)
